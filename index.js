cce0dd0f851ed2664b3dfacc68d3193d0b68b3de
John Yang <john.yang8097@gmail.com>
2022-11-02 15:18:19 -0400

    commit tree dcc17bed054a33f1632c7d033828c4343beea83f

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400) {
+  return `This one is on me!`
 }
+  else if (400 < value && value < 2000) {
+    return `That will be twenty bucks.`
+  }
+  else if (2000 < value && value < 2500) {
+    return `I will gladly take your thirty bucks.`
+  }
+  else if (value > 2500) {
+    return `No can do.`
+  }
+}
+// in order to declare a value in between two values, use &&
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === `NYC` ? `Ok, sounds good.` : `No go.`
 }
+// make sure to define what the city is
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
+// when using the switch function, make sure to use ':' when determining the return
